32f91f65c4972ccc2d2e4b6cab05a87f9dabecf9
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-12-01 10:42:10 +0100

    01: Solution part 1

--- 2022/aoc01/src/main.rs
+++ 2022/aoc01/src/main.rs
@@ -9,5 +9,19 @@ fn main() {
         file.read_to_string(&mut input).unwrap();
     }
 
-    print!("{}", input);
+    let mut elves = Vec::new();
+    let mut new_elf = 0;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            elves.push(new_elf);
+            new_elf = 0;
+        } else {
+            new_elf += line.parse::<u32>().unwrap();
+        }
+    }
+
+    elves.sort();
+
+    println!("{}", elves[elves.len() - 1]);
 }
