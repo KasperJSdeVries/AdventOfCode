dca7fa181f1a12ec01aa04d66db3328cff08136c
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-12-01 09:22:37 +0100

    01: Read the input file

--- 2022/aoc01/src/main.rs
+++ 2022/aoc01/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    {
+        let mut file = File::open("input.txt").unwrap();
+        file.read_to_string(&mut input).unwrap();
+    }
+
+    print!("{}", input);
 }
